5e9ed79e04971578e7e9bf04acc01fe097046ee8
Ben Summers <ben.summers@ib1.org>
2024-09-04 11:31:29 +0100

    Encode Roles and Application URL in x509 certificate extensions (#6)
    Directory and Registry URLs are encoded in x509 v3 certificate extensions
    using IB1 OIDs. These do not have the 64 character limit when encoding them
    in the Subject, which allows for descriptive URLs to be used.
    
    ---------
    
    Co-authored-by: Kip Parker <kip@fifthcontinent.io>

--- resource/tests/test_auth.py
+++ resource/tests/test_auth.py
@@ -2,39 +2,68 @@ import pytest
 
 from fastapi.exceptions import HTTPException
 
-from tests import cert_response
+from tests import client_certificate
 from api.main import require_role
 
 
 def test_role_in_certificate():
-    cert_urlencoded = cert_response(cert_file="cert1.pem", urlencoded=True)
-    require_role("supply-voltage-reader@electricity", cert_urlencoded)
-    require_role("reporter@electricity", cert_urlencoded)
+    test_role = (
+        "https://registry.estf.ib1.org/scheme/electricty/role/supply-voltage-reader"
+    )
+    cert_urlencoded = client_certificate(
+        roles=[
+            test_role,
+            "https://registry.estf.ib1.org/scheme/electricty/group/reporter",
+        ]
+    )
+    require_role(  # No assertion, just checking for exceptions
+        test_role,
+        cert_urlencoded,
+    )
 
 
 def test_role_not_in_certificate():
-    cert_urlencoded = cert_response(cert_file="cert2.pem", urlencoded=True)
+    test_role = (
+        "https://registry.estf.ib1.org/scheme/electricty/role/supply-voltage-reader"
+    )
+    cert_urlencoded = client_certificate(
+        roles=[
+            "https://registry.estf.ib1.org/scheme/electricty/group/reporter",
+        ]
+    )
     with pytest.raises(HTTPException):
-        require_role("supply-voltage-reader@electricity", cert_urlencoded)
+        require_role(test_role, cert_urlencoded)
 
 
 def test_certificate_with_no_roles():
-    cert_urlencoded = cert_response(cert_file="cert3.pem", urlencoded=True)
+    cert_urlencoded = client_certificate()
     with pytest.raises(HTTPException):
-        require_role("supply-voltage-reader@electricity", cert_urlencoded)
+        require_role(
+            "https://registry.estf.ib1.org/scheme/electricty/role/supply-voltage-reader",
+            cert_urlencoded,
+        )
 
 
 def test_empty_role():
-    cert_urlencoded = cert_response(cert_file="cert1.pem", urlencoded=True)
+    cert_urlencoded = client_certificate(roles=[""])
     with pytest.raises(HTTPException):
-        require_role("", cert_urlencoded)
+        require_role(
+            "https://registry.estf.ib1.org/scheme/electricty/role/supply-voltage-reader",
+            cert_urlencoded,
+        )
 
 
 def test_no_certificate_supplied():
     with pytest.raises(HTTPException):
-        require_role("supply-voltage-reader@electricity", None)
+        require_role(
+            "https://registry.estf.ib1.org/scheme/electricty/role/supply-voltage-reader",
+            None,
+        )
 
 
 def test_bad_certificate():
     with pytest.raises(ValueError):
-        require_role("supply-voltage-reader@electricity", "Not a PEM encoded certificate")
+        require_role(
+            "https://registry.estf.ib1.org/scheme/electricty/role/supply-voltage-reader",
+            "Not a PEM encoded certificate",
+        )
